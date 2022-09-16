814e5d64c7e853cd9c5a68fea32dd1333e11ef49
Jacob Patton <jacob@trisignia.com>
2022-09-16 18:04:31 -0400

    Refactor the user_test to add a setup method

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,20 +1,23 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
+  def setup
+    @user = User.new(name: 'John', email: 'john@example.com')
+  end
+
   test 'valid user' do
-    user = User.new(name: 'John', email: 'john@example.com')
-    assert user.valid?, 'User should be valid with name and email'
+    assert @user.valid?, '@user should be valid with name and email'
   end
 
   test 'invalid without name' do
-    user = User.new(email: 'john@example.com')
-    refute user.valid?, 'Failure message.'
-    assert_not_nil user.errors[:name], 'no name validation errors found'
+    @user.name = nil
+    refute @user.valid?, 'Failure message.'
+    assert_not_nil @user.errors[:name], 'no name validation errors found'
   end
 
   test 'invalid without email' do
-    user = User.new(name: 'John Doe')
-    refute user.valid?, 'Failure message.'
-    assert_not_nil user.errors[:email], 'no email validation errors found'
+    @user.email = nil
+    refute @user.valid?, 'Failure message.'
+    assert_not_nil @user.errors[:email], 'no email validation errors found'
   end
 end
